f4ccbff87a9eb9d71de596bf80c1a041f5a8efb3
Wil <eddyw@wit.edu>
2021-02-11 16:49:36 -0500

    Added blog post tables to db, changed front end to show db queries

--- lazy_birder/dashbird/models.py
+++ lazy_birder/dashbird/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
--- lazy_birder/dashbird/views.py
+++ lazy_birder/dashbird/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Wil Eddy',
-        'title': 'Blog post',
-        'content': 'First post',
-        'date_posted': 'May 6, 2021'
-    },
-    {
-        'author': 'Wil Eddy',
-        'title': 'Blog post 2',
-        'content': 'Second post',
-        'date_posted': 'May 8, 2021'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'dashbird/home.html', context)
 
