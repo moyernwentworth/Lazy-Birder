036401d789cea74854ca84881166a80f87e2f5db
Wil <eddyw@wit.edu>
2021-02-16 12:04:50 -0500

    Fixed regsitration page

--- lazy_birder/users/models.py
+++ lazy_birder/users/models.py
@@ -9,14 +9,14 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
+    def save(self, *args, **kwargs):
         """After the model gets saved, use exisiting function and ensure resizing"""
-        super().save()
+        super(Profile, self).save(*args, **kwargs)
 
         img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
-            img.save()
+            img.save(self.image.path)
         
